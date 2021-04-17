1ca9fe8f9b38c57d30795bb88fe4e25218292446
kechankrisna <ke.chankrisna168@gmail.com>
2021-04-17 19:00:00 +0700

    extend webview component on native platformview

---
+++ android/src/main/kotlin/app/mylekha/webcontent_converter/FLNativeViewFactory.kt
@@ -0,0 +1,4 @@
+package app.mylekha.webcontent_converter
+
+class FLNativeViewFactory {
+}
---
+++ ios/Classes/FLWebView.swift
@@ -0,0 +1,8 @@
+//
+//  FLWebView.swift
+//  webcontent_converter
+//
+//  Created by whitehat on 17/4/21.
+//
+
+import Foundation
