10f7f8e63c374dc7f660e74f5ef06277207e1c8a
kechankrisna <ke.chankrisna168@gmail.com>
2021-06-08 16:44:36 +0700

    updated example

--- example/lib/screens/content_pdf_screen.dart
+++ example/lib/screens/content_pdf_screen.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:pdf/pdf.dart';
 import 'package:printing/printing.dart';
 import 'package:webcontent_converter/webcontent_converter.dart';
 import 'package:webcontent_converter_example/services/demo.dart';
