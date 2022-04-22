93291731bc02b2e35e4fb50c90ec08b99a81a68c
kechankrisna <ke.chankrisna168@gmail.com>
2022-04-22 17:22:07 +0700

    fixed latest pupeteer

--- lib/webview_helper.dart
+++ lib/webview_helper.dart
@@ -35,17 +35,17 @@ class WebViewHelper {
     var paths = [];
     if (io.Platform.isWindows) {
       paths = [
-        "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
         "C:\\ProgramFiles(x86)\\Google\\Chrome\\Application\\chrome.exe",
+        "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
         "C:\\Program Files\\Mozilla Firefox\\firefox.exe",
         io.Directory("chromium").absolute.path,
       ];
     } else if (io.Platform.isMacOS) {
       paths = [
         // "/Applications/Safari.app/Contents/MacOS/SafariForWebKitDevelopment",
-        "/Applications/Microsoft\ Edge\ Canary.app/Contents/MacOS/Microsoft\ Edge\ Canary",
         "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome",
         "/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary",
+        "/Applications/Microsoft\ Edge\ Canary.app/Contents/MacOS/Microsoft\ Edge\ Canary",
         "/Applications/Chromium.app/Contents/MacOS/Chromium",
         "/Applications/Firefox.app/Contents/MacOS/firefox",
         "/Applications/Firefox.app/Contents/MacOS/firefox-bin",
