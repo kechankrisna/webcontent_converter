83bf3c81a673467f17945e15c59c23aefc930187
Phong Phong <phong2a63@gmail.com>
2022-06-22 10:57:17 +0700

    fix Nullable Context for Flutter Android 3.0.2

--- android/src/main/kotlin/app/mylekha/webcontent_converter/FLNativeViewFactory.kt
+++ android/src/main/kotlin/app/mylekha/webcontent_converter/FLNativeViewFactory.kt
@@ -1,5 +1,6 @@
 package app.mylekha.webcontent_converter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.View
 import android.webkit.WebView
@@ -8,27 +9,27 @@ import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 
 class FLNativeViewFactory : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
-    override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
+    override fun create(context: Context?, viewId: Int, args: Any?): PlatformView {
         val creationParams = args as Map<String?, Any?>?
-        return FLNativeView(context, viewId, creationParams)
+        return FLNativeView(context!!, viewId, creationParams)
     }
 }
 
 
-internal class FLNativeView(context: Context, id: Int, creationParams: Map<String?, Any?>?) : PlatformView {
+@SuppressLint("SetJavaScriptEnabled")
+internal class FLNativeView(context: Context, id: Int, creationParams: Map<String?, Any?>?) :
+    PlatformView {
     private val webView: WebView = WebView(context)
     private var arguments: Map<String?, Any?>? = creationParams
 
-    override fun getView(): View {
-        return webView
-    }
+    override fun getView(): View = webView
 
     override fun dispose() {}
 
     init {
-        var width = (arguments!!["width"]!! as Number).toInt()
-        var height = (arguments!!["height"]!! as Number).toInt()
-        var content = arguments!!["content"] as String
+        val width = (arguments!!["width"]!! as Number).toInt()
+        val height = (arguments!!["height"]!! as Number).toInt()
+        val content = arguments!!["content"] as String
         webView.layout(0, 0, width, height)
         webView.loadDataWithBaseURL(null, content, "text/HTML", "UTF-8", null)
         webView.setInitialScale(1)
@@ -36,8 +37,6 @@ internal class FLNativeView(context: Context, id: Int, creationParams: Map<Strin
         webView.settings.useWideViewPort = true
         webView.settings.javaScriptCanOpenWindowsAutomatically = true
         webView.settings.loadWithOverviewMode = true
-
-
     }
 
 }
