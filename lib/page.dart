724afe8e6467fa45a24caea5179acfee8bdb7dad
Volodymyr Rudenok <Volodymyr.Rudenok@dazn.com>
2024-07-22 13:43:11 +0200

    Page: landscape support

--- lib/page.dart
+++ lib/page.dart
@@ -5,69 +5,59 @@ num _cmToInches(num cm) => _pxToInches(cm / 37.8);
 num _mmToInches(num mm) => _cmToInches(mm / 10);
 
 class PaperFormat {
-  static const letter = PaperFormat.inches(width: 8.5, height: 11);
-  static const legal = PaperFormat.inches(width: 8.5, height: 14);
-  static const tabloid = PaperFormat.inches(width: 11, height: 17);
-  static const ledger = PaperFormat.inches(width: 17, height: 11);
-  static const a0 = PaperFormat.inches(width: 33.1, height: 46.8);
-  static const a1 = PaperFormat.inches(width: 23.4, height: 33.1);
-  static const a2 = PaperFormat.inches(width: 16.54, height: 23.4);
-  static const a3 = PaperFormat.inches(width: 11.7, height: 16.54);
-  static const a4 = PaperFormat.inches(width: 8.27, height: 11.7);
-  static const a5 = PaperFormat.inches(width: 5.83, height: 8.27);
-  static const a6 = PaperFormat.inches(width: 4.13, height: 5.83);
+  static PaperFormat a3({required bool isPortrait}) =>
+      PaperFormat.inches(width: 11.69, height: 16.54, isPortrait: isPortrait);
+  static PaperFormat a4({required bool isPortrait}) =>
+      PaperFormat.inches(width: 8.27, height: 11.69, isPortrait: isPortrait);
+  static PaperFormat a5({required bool isPortrait}) =>
+      PaperFormat.inches(width: 5.83, height: 8.27, isPortrait: isPortrait);
+  static PaperFormat b4({required bool isPortrait}) =>
+      PaperFormat.inches(width: 9.84, height: 13.90, isPortrait: isPortrait);
+  static PaperFormat b5({required bool isPortrait}) =>
+      PaperFormat.inches(width: 6.93, height: 9.84, isPortrait: isPortrait);
+  static PaperFormat executive({required bool isPortrait}) =>
+      PaperFormat.inches(width: 7.25, height: 10.5, isPortrait: isPortrait);
+  static PaperFormat legal({required bool isPortrait}) =>
+      PaperFormat.inches(width: 8.5, height: 14, isPortrait: isPortrait);
+  static PaperFormat letter({required bool isPortrait}) =>
+      PaperFormat.inches(width: 8.5, height: 11, isPortrait: isPortrait);
+  static PaperFormat tabloid({required bool isPortrait}) =>
+      PaperFormat.inches(width: 11, height: 17, isPortrait: isPortrait);
 
-  /// [value] is paper size in string | default: a4
-  /// Example : `PaperFormat.fromString("a4")`
-  factory PaperFormat.fromString(String value) {
-    switch (value.toLowerCase()) {
-      case "letter":
-        return PaperFormat.letter;
-      case "legal":
-        return PaperFormat.legal;
-      case "tabloid":
-        return PaperFormat.tabloid;
-      case "ledger":
-        return PaperFormat.ledger;
-      case "a0":
-        return PaperFormat.a0;
-      case "a1":
-        return PaperFormat.a1;
-      case "a2":
-        return PaperFormat.a2;
-      case "a3":
-        return PaperFormat.a3;
-      case "a4":
-        return PaperFormat.a4;
-      case "a5":
-        return PaperFormat.a5;
-      case "a6":
-        return PaperFormat.a6;
-      default:
-        return PaperFormat.a4;
-    }
-  }
+  const PaperFormat.inches({
+    required this.width,
+    required this.height,
+    required this.isPortrait,
+  });
 
   final num width, height;
+  final bool isPortrait;
 
-  const PaperFormat.inches({required this.width, required this.height});
-
-  PaperFormat.px({required int width, required int height})
-      : width = _pxToInches(width),
+  PaperFormat.px({
+    required int width,
+    required int height,
+    required this.isPortrait,
+  })  : width = _pxToInches(width),
         height = _pxToInches(height);
 
-  PaperFormat.cm({required num width, required num height})
-      : width = _cmToInches(width),
+  PaperFormat.cm({
+    required num width,
+    required num height,
+    required this.isPortrait,
+  })  : width = _cmToInches(width),
         height = _cmToInches(height);
 
-  PaperFormat.mm({required num width, required num height})
-      : width = _mmToInches(width),
+  PaperFormat.mm({
+    required num width,
+    required num height,
+    required this.isPortrait,
+  })  : width = _mmToInches(width),
         height = _mmToInches(height);
 
   Map<String, num> toMap() {
     return {
-      "width": width,
-      "height": height,
+      "width": isPortrait ? width : height,
+      "height": isPortrait ? height : width,
     };
   }
 
@@ -89,8 +79,7 @@ class PdfMargins {
         right = right ?? 0;
 
   /// [PdfMargins.px] then return in Inches
-  factory PdfMargins.px(
-      {int top = 0, int bottom = 0, int left = 0, int right = 0}) {
+  factory PdfMargins.px({int top = 0, int bottom = 0, int left = 0, int right = 0}) {
     return PdfMargins.inches(
       top: _pxToInches(top),
       bottom: _pxToInches(bottom),
@@ -100,8 +89,7 @@ class PdfMargins {
   }
 
   /// [PdfMargins.cm] then return in Inches
-  factory PdfMargins.cm(
-      {num top = 0, num bottom = 0, num left = 0, num right = 0}) {
+  factory PdfMargins.cm({num top = 0, num bottom = 0, num left = 0, num right = 0}) {
     return PdfMargins.inches(
       top: _cmToInches(top),
       bottom: _cmToInches(bottom),
@@ -111,8 +99,7 @@ class PdfMargins {
   }
 
   /// [PdfMargins.mm] then return in Inches
-  factory PdfMargins.mm(
-      {num top = 0, num bottom = 0, num left = 0, num right = 0}) {
+  factory PdfMargins.mm({num top = 0, num bottom = 0, num left = 0, num right = 0}) {
     return PdfMargins.inches(
       top: _mmToInches(top),
       bottom: _mmToInches(bottom),
@@ -131,6 +118,5 @@ class PdfMargins {
   }
 
   @override
-  String toString() =>
-      'PdfMargins.inches(top: $top, bottom: $bottom, left: $left, right: $right)';
+  String toString() => 'PdfMargins.inches(top: $top, bottom: $bottom, left: $left, right: $right)';
 }
