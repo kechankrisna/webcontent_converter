c9d05e834b7cab8ef17169baed46f01787293cf5
Volodymyr Rudenok <Volodymyr.Rudenok@dazn.com>
2024-07-22 13:44:29 +0200

    iOS: footer and header, removed old version code

--- ios/Classes/SwiftWebcontentConverterPlugin.swift
+++ ios/Classes/SwiftWebcontentConverterPlugin.swift
@@ -31,31 +31,13 @@ public class SwiftWebcontentConverterPlugin: NSObject, FlutterPlugin {
             var bytes = FlutterStandardTypedData.init(bytes: Data() )
             urlObservation = webView.observe(\.isLoading, changeHandler: { (webView, change) in
                 DispatchQueue.main.asyncAfter(deadline: .now() + (duration!/10000) ) {
-                    print("height = \(self.webView.scrollView.contentSize.height)")
-                    print("width = \(self.webView.scrollView.contentSize.width)")
-                    if #available(iOS 11.0, *) {
-                        let configuration = WKSnapshotConfiguration()
-                        var size = self.webView.scrollView.contentSize
-                        size.height = size.height + 50
-                        print("height = \(size.height)")
-                        configuration.rect = CGRect(origin: .zero, size: size)
-                        self.webView.snapshotView(afterScreenUpdates: true)
-                        self.webView.takeSnapshot(with: configuration) { (image, error) in
-                            guard let data = image!.jpegData(compressionQuality: 1) else {
-                                result( bytes )
-                                self.dispose()
-                                return
-                            }
-                            bytes = FlutterStandardTypedData.init(bytes: data)
-                            result(bytes)
-                            // UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
-                            //dispose
-                            self.dispose()
-                            print("Got snapshot")
-                        }
-                    } else if #available(iOS 9.0, *) {
-                        
-                        let image =  self.webView.snapshot()
+                    let configuration = WKSnapshotConfiguration()
+                    var size = self.webView.scrollView.contentSize
+                    size.height = size.height + 50
+                    print("height = \(size.height)")
+                    configuration.rect = CGRect(origin: .zero, size: size)
+                    self.webView.snapshotView(afterScreenUpdates: true)
+                    self.webView.takeSnapshot(with: configuration) { (image, error) in
                         guard let data = image!.jpegData(compressionQuality: 1) else {
                             result( bytes )
                             self.dispose()
@@ -63,15 +45,6 @@ public class SwiftWebcontentConverterPlugin: NSObject, FlutterPlugin {
                         }
                         bytes = FlutterStandardTypedData.init(bytes: data)
                         result(bytes)
-                        // UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
-                        //dispose
-                        self.dispose()
-                        print("Got snapshot")
-                        
-                        
-                    }else {
-                        
-                        result( bytes )
                         self.dispose()
                     }
                     
@@ -92,17 +65,11 @@ public class SwiftWebcontentConverterPlugin: NSObject, FlutterPlugin {
                 DispatchQueue.main.asyncAfter(deadline: .now() + (duration!/10000) ) {
                     print("height = \(self.webView.scrollView.contentSize.height)")
                     print("width = \(self.webView.scrollView.contentSize.width)")
-                    if #available(iOS 11.0, *) {
-                        let configuration = WKSnapshotConfiguration()
-                        configuration.rect = CGRect(x: 0, y: 0, width: CGFloat(format!["width"] ?? 8.27).toPixel(), height: CGFloat(format!["height"] ?? 11.27).toPixel() )
-                        guard let path = self.webView.exportAsPdfFromWebView(savedPath: savedPath!, format: format!, margins: margins!) else {
-                            result(nil)
-                            return
-                        }
-                        result(path)
-                    }else{
+                    guard let path = self.webView.exportAsPdfFromWebView(savedPath: savedPath!, format: format!, margins: margins!) else {
                         result(nil)
+                        return
                     }
+                    result(path)
                     //dispose
                     self.dispose()
                 }
@@ -119,11 +86,8 @@ public class SwiftWebcontentConverterPlugin: NSObject, FlutterPlugin {
             self.webView.loadHTMLString(content!, baseURL: baseURL)// load html into hidden webview
             urlObservation = webView.observe(\.isLoading, changeHandler: { (webView, change) in
                 DispatchQueue.main.asyncAfter(deadline: .now() + (duration!/10000) ) {
-                    print("height = \(self.webView.scrollView.contentSize.height)")
-                    print("width = \(self.webView.scrollView.contentSize.width)")
                     self.createWebPrintJob(webView: webView)
                     result(nil)
-                    //dispose
                     self.dispose()
                 }
             })
@@ -157,11 +121,9 @@ public class SwiftWebcontentConverterPlugin: NSObject, FlutterPlugin {
         if let viewWithTag = self.webView.viewWithTag(100) {
             viewWithTag.removeFromSuperview() // remove hidden webview when pdf is generated
             // clear WKWebView cache
-            if #available(iOS 9.0, *) {
-                WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
-                    records.forEach { record in
-                        WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
-                    }
+            WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
+                records.forEach { record in
+                    WKWebsiteDataStore.default().removeData(ofTypes: record.dataTypes, for: [record], completionHandler: {})
                 }
             }
         }
@@ -196,7 +158,7 @@ extension WKWebView {
         formatter.perPageContentInsets = UIEdgeInsets(top: CGFloat(margins["top"] ?? 0).toPixel(), left:CGFloat(margins["left"] ?? 0).toPixel(), bottom: CGFloat(margins["bottom"] ?? 0).toPixel(), right: CGFloat(margins["right"] ?? 0).toPixel() )
         let page = CGRect(x: 0, y: 0, width: CGFloat(format["width"] ?? 8.27).toPixel(), height: CGFloat(format["height"] ?? 11.27).toPixel() )
         let printable = page.insetBy(dx: 0, dy: 0)
-        let render = UIPrintPageRenderer()
+        let render = CustomPrintPageRenderer(headerText: "47-0412 Faith is Substance - English", footerText: "Page")
         render.addPrintFormatter(formatter, startingAtPageAt: 0)
         render.setValue(NSValue(cgRect: page), forKey: "paperRect")
         render.setValue(NSValue(cgRect: printable), forKey: "printableRect")
@@ -217,9 +179,74 @@ extension WKWebView {
     }
 }
 
-// render format
-extension UIPrintPageRenderer {
+// used convert current inches value into real CGFloat
+extension CGFloat{
+    func toPixel() -> CGFloat {
+        if(self>0){
+            return self * 96
+        }
+        return 0
+    }
+}
+
+class CustomPrintPageRenderer: UIPrintPageRenderer {
     
+    var headerHeightValue: CGFloat
+    var footerHeightValue: CGFloat
+
+    let headerText: String
+    let footerText: String
+
+    init(headerText: String, footerText: String, headerHeightValue: CGFloat = 50.0, footerHeightValue: CGFloat = 50.0) {
+        self.headerText = headerText
+        self.footerText = footerText
+        self.headerHeightValue = headerHeightValue
+        self.footerHeightValue = footerHeightValue
+    }
+    
+    override var headerHeight: CGFloat {
+        get {
+            headerHeightValue
+        }
+        set {
+            headerHeightValue = newValue
+        }
+    }
+    
+    override var footerHeight: CGFloat {
+        get {
+            footerHeightValue
+        }
+        set {
+            footerHeightValue = newValue
+        }
+    }
+    
+    override func drawHeaderForPage(at pageIndex: Int, in headerRect: CGRect) {
+        let attributes = [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12),
+            NSAttributedString.Key.foregroundColor: UIColor.gray
+        ]
+
+        let textSize = headerText.size(withAttributes: attributes)
+        let textRect = CGRect(x: headerRect.midX - textSize.width / 2, y: headerRect.midY - textSize.height / 2, width: textSize.width, height: textSize.height)
+
+        headerText.draw(in: textRect, withAttributes: attributes)
+    }
+    
+    override func drawFooterForPage(at pageIndex: Int, in footerRect: CGRect) {
+        let footerText = "\(footerText) \(pageIndex + 1)"
+        let attributes = [
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12),
+            NSAttributedString.Key.foregroundColor: UIColor.darkGray
+        ]
+
+        let textSize = footerText.size(withAttributes: attributes)
+        let textRect = CGRect(x: footerRect.midX - textSize.width / 2, y: footerRect.midY - textSize.height / 2, width: textSize.width, height: textSize.height)
+
+        footerText.draw(in: textRect, withAttributes: attributes)
+    }
+
     func generatePdfData() -> NSMutableData {
         let pdfData = NSMutableData()
         UIGraphicsBeginPDFContextToData(pdfData, self.paperRect, nil)
@@ -231,15 +258,5 @@ extension UIPrintPageRenderer {
         }
         UIGraphicsEndPDFContext();
         return pdfData
-    }
-}
-
-// used convert current inches value into real CGFloat
-extension CGFloat{
-    func toPixel() -> CGFloat {
-        if(self>0){
-            return self * 96
-        }
-        return 0
     }
 }
